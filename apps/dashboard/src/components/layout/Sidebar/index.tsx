d25fde29cd165d7b62ef9adc7f7d5105506f7657
Gabriel Fachini <gabriel.fm.fachini@gmail.com>
2025-10-24 20:36:16 -0300

    refactor: Remover gestos mobile da sidebar e hooks relacionados

--- apps/dashboard/src/components/layout/Sidebar/index.tsx
+++ apps/dashboard/src/components/layout/Sidebar/index.tsx
@@ -1,12 +1,10 @@
 import { SidebarDesktop } from './SidebarDesktop'
 import { SidebarMobile } from './SidebarMobile'
 import { MobileHeader } from './MobileHeader'
-import { MobileGestureArea } from './MobileGestureArea'
 
 export const Sidebar = () => {
   return (
     <>
-      <MobileGestureArea />
       <MobileHeader />
       <SidebarDesktop />
       <SidebarMobile />
